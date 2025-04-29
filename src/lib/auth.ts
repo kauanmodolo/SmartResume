e4ccab41c630080757f9316a2f0f923196c5f80a
kauanmodolo <kaucraft@gmail.com>
2025-04-29 14:50:26 -0300

    fix: auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,10 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
-import {DrizzleAdapter} from "@auth/drizzle-adapter"
-import { db } from "@/db/drizzle"
- 
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/db/drizzle";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db),
   providers: [GitHub, Google],
@@ -12,7 +13,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   callbacks: {
     authorized: async ({ auth }) => {
-        return !!auth;
-    }
-  }
-})
+      return !!auth;
+    },
+  },
+});
